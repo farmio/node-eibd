b204f6e90f4c79c84cb758bef2bcb8d514f205a9
farmio <meti@meti-BC.localdomain>
2018-08-13 09:20:27 +0200

    DPT 15, Tests
    - fixed DPT 15
    - cleaned up tests

--- test/encoder.test.js
+++ test/encoder.test.js
@@ -1,12 +1,18 @@
 'use strict';
 
-var assert = require('assert'),
-    Encoder = require('../lib/encoder.js'),
-    tools = require('../lib/tools.js');
+const assert = require('assert'),
+      Encoder = require('../lib/encoder.js'),
+      tools = require('../lib/tools.js');
 
-var enc = null;
+/**
+* Tests encoder methods for dpt types
+* Encoded values should equal values encoded by ETS
+*/
 
-//TODO: 6, 8, 12, 13
+let enc = null;
+let buffer;
+
+//TODO: 6.020
 
 describe('Encoder', function() {
 
@@ -14,9 +20,9 @@ describe('Encoder', function() {
     enc = new Encoder();
   });
 
-  it('should not encode DPTNOTIMPLEMENTED', function() {
+  it('should not encode unimplemented DPT', function() {
     const dpt = tools.dptParse('DPT0');
-    var buffer = enc.encode(dpt, -5.08);
+    buffer = enc.encode(dpt, -5.08);
     assert.equal(buffer, undefined);
   });
 
@@ -24,7 +30,7 @@ describe('Encoder', function() {
 
     it('should encode DPT1 value', function() {
       const dpt = tools.dptParse('DPT1');
-      var buffer = enc.encode(dpt,0);
+      buffer = enc.encode(dpt,0);
       assert.equal(buffer.readUInt8(0), 0);
 
       buffer = enc.encode(dpt,1);
@@ -39,7 +45,7 @@ describe('Encoder', function() {
 
     it('should encode DPT2 values', function() {
       const dpt = tools.dptParse('DPT2');
-      var buffer = enc.encode(dpt, {control: false, value: false});
+      buffer = enc.encode(dpt, {control: false, value: false});
       assert.equal(buffer.readUInt8(0), 0);
 
       buffer = enc.encode(dpt, {control: true, value: true});
@@ -54,7 +60,7 @@ describe('Encoder', function() {
 
     it('should encode DPT3 values', function() {
       const dpt = tools.dptParse('DPT3');
-      var buffer = enc.encode(dpt, {control: true, step: 7});
+      buffer = enc.encode(dpt, {control: true, step: 7});
       assert.equal(buffer.readUInt8(0), 0xF);
 
       buffer = enc.encode(dpt, {control: true, step: 4});
@@ -73,33 +79,47 @@ describe('Encoder', function() {
 
     it('should encode DPT4.001 values', function() {
       const dpt = tools.dptParse('DPT4.001');
-      var buffer = enc.encode(dpt,'a');
+      buffer = enc.encode(dpt,'a');
       assert.equal(buffer.readUInt8(0), 0x61);
     });
 
     it('should encode DPT4.002 values', function() {
       const dpt = tools.dptParse('DPT4.002');
-      var buffer = enc.encode(dpt,'ö');
+      buffer = enc.encode(dpt,'ö');
       assert.equal(buffer.readUInt8(0), 0xF6);
     });
 
     it('should encode DPT5 values', function() {
       const dpt = tools.dptParse('DPT5');
-      var buffer = enc.encode(dpt,40);
+      buffer = enc.encode(dpt,40);
       assert.equal(buffer.readUInt8(0), 40);
     });
 
-    it('should encode DPT6 values');
+    it('should encode DPT6 values', function() {
+      const dpt = tools.dptParse('DPT6');
+      buffer = enc.encode(dpt,-128);
+      assert.equal(buffer.readUInt8(0), 0x80);
+
+      buffer = enc.encode(dpt,0);
+      assert.equal(buffer.readUInt8(0), 0x00);
+
+      buffer = enc.encode(dpt,40);
+      assert.equal(buffer.readUInt8(0), 0x28);
+
+      buffer = enc.encode(dpt,127);
+      assert.equal(buffer.readUInt8(0), 0x7F);
+    });
 
     it('should encode DPT6.020 values');
 
   });
 
   describe('2 byte Encoder', function() {
+    const dpt9 = tools.dptParse('DPT9');
 
     it('should encode DPT7 values', function() {
       const dpt = tools.dptParse('DPT7');
-      var buffer = enc.encode(dpt,0);
+      buffer = enc.encode(dpt,0);
       assert.equal(buffer.readUInt16BE(0), 0x0000);
 
       buffer = enc.encode(dpt,2550);
@@ -112,51 +132,52 @@ describe('Encoder', function() {
       assert.equal(buffer.readUInt16BE(0), 0xFFFF);
     });
  
-    it('should encode DPT8 values');
+    it('should encode DPT8 values', function() {
+      const dpt = tools.dptParse('DPT8');
+      buffer = enc.encode(dpt,-32);
+      assert.equal(buffer.readUInt16BE(0), 0xFFE0);
+
+      buffer = enc.encode(dpt,-32768);
+      assert.equal(buffer.readUInt16BE(0), 0x8000);
+
+      buffer = enc.encode(dpt,32767);
+      assert.equal(buffer.readUInt16BE(0), 0x7FFF);
+
+      buffer = enc.encode(dpt,127);
+      assert.equal(buffer.readUInt16BE(0), 0x007F);
+    });
 
     it('should encode DPT9 values', function() {
-      const dpt = tools.dptParse('DPT9');
-      var buffer = enc.encode(dpt,40);
-      var value = buffer.readUInt16BE(0);
-      assert.equal(value, 0x0FD0); // was: 0x13e8
+      buffer = enc.encode(dpt9,40);
+      assert.equal(buffer.readUInt16BE(0), 0x0FD0); // was: 0x13e8
 
-      buffer = enc.encode(dpt,50);
-      value = buffer.readUInt16BE(0);
-      assert.equal(value, 0x14e2);
+      buffer = enc.encode(dpt9,50);
+      assert.equal(buffer.readUInt16BE(0), 0x14e2);
     });
 
     it('should encode DPT9 floating values', function() {
-      const dpt = tools.dptParse('DPT9');
-      var buffer = enc.encode(dpt,20.2);
-      var value = buffer.readUInt16BE(0);
-      assert.equal(value, 0x07E4); // was 0x11F9
+      buffer = enc.encode(dpt9,20.2);
+      assert.equal(buffer.readUInt16BE(0), 0x07E4); // was 0x11F9
 
-      buffer = enc.encode(dpt,20.2);
-      value = buffer.readUInt16BE(0);
-      assert.equal(value, 0x07E4);
+      buffer = enc.encode(dpt9,20.2);
+      assert.equal(buffer.readUInt16BE(0), 0x07E4);
     });
 
     it('should encode DPT9 negative values', function() {
-      const dpt = tools.dptParse('DPT9');
-      var buffer = enc.encode(dpt,-100.32);
-      var value = buffer.readUInt16BE(0);
-      assert.equal(value, 0x9B1A);
+      buffer = enc.encode(dpt9,-100.32);
+      assert.equal(buffer.readUInt16BE(0), 0x9B1A);
 
-      buffer = enc.encode(dpt,-175.84);
-      value = buffer.readUInt16BE(0);
-      assert.equal(value, 0xA3B5);
+      buffer = enc.encode(dpt9,-175.84);
+      assert.equal(buffer.readUInt16BE(0), 0xA3B5);
 
-      buffer = enc.encode(dpt,-199.52);
-      value = buffer.readUInt16BE(0);
-      assert.equal(value, 0xA321);
+      buffer = enc.encode(dpt9,-199.52);
+      assert.equal(buffer.readUInt16BE(0), 0xA321);
 
-      buffer = enc.encode(dpt,-200.32);
-      value = buffer.readUInt16BE(0);
-      assert.equal(value, 0xA31C); // was: 0xB6C7
+      buffer = enc.encode(dpt9,-200.32);
+      assert.equal(buffer.readUInt16BE(0), 0xA31C); // was: 0xB6C7
 
-      buffer = enc.encode(dpt,-5.08);
-      value = buffer.readUInt16BE(0);
-      assert.equal(value, 0x8604);
+      buffer = enc.encode(dpt9,-5.08);
+      assert.equal(buffer.readUInt16BE(0), 0x8604);
     });
 
   });
@@ -187,14 +208,14 @@ describe('Encoder', function() {
     it('should encode DPT11 values', function(done) {
       const dpt = tools.dptParse('DPT11');
 
-      var day = 2; // February -> 010
-      var month = 10; // 10 day  -> A
-      var year = 14; // 14 -> 0xE -> 00001110
+      const day = 2; // February -> 010
+      const month = 10; // 10 day  -> A
+      const year = 14; // 14 -> 0xE -> 00001110
 
-      var buffer = enc.encode(dpt, new Date(2000 + year, month -1, day));
-      var value0 = buffer.readUInt8(0); // Read the first octect
-      var value1 = buffer.readUInt8(1); // Read the second octect
-      var value2 = buffer.readUInt8(2);  // Read the third octect
+      buffer = enc.encode(dpt, new Date(2000 + year, month -1, day));
+      const value0 = buffer.readUInt8(0); // Read the first octect
+      const value1 = buffer.readUInt8(1); // Read the second octect
+      const value2 = buffer.readUInt8(2);  // Read the third octect
 
       assert.equal(value0, 0x02); // 00000010 = 2
       assert.equal(value1, 0x0A); // 10 = 00001010 = A
@@ -207,39 +228,69 @@ describe('Encoder', function() {
 
   describe('4 byte Encoder', function() {
     const dpt14 = tools.dptParse('DPT14');
-    let buf2str = function (buf) {
-      return buf.toString('hex').toUpperCase();
-    }
 
-    it('should encode DPT12 values');
+    it('should encode DPT12 values', function() {
+      const dpt = tools.dptParse('DPT12');
+      buffer = enc.encode(dpt,0);
+      assert.equal(buffer.readUInt32BE(0), 0x00000000);
 
-    it('should encode DPT13 values');
+      buffer = enc.encode(dpt,1233333333);
+      assert.equal(buffer.readUInt32BE(0), 0x49832C55);
+
+      buffer = enc.encode(dpt,4294967295);
+      assert.equal(buffer.readUInt32BE(0), 0xFFFFFFFF);
+
+      buffer = enc.encode(dpt,15);
+      assert.equal(buffer.readUInt32BE(0), 0x0000000F);
+    });
+
+    it('should encode DPT13 values', function() {
+      const dpt = tools.dptParse('DPT13');
+      buffer = enc.encode(dpt,0);
+      assert.equal(buffer.readUInt32BE(0), 0x00000000);
+
+      buffer = enc.encode(dpt,2147483647);
+      assert.equal(buffer.readUInt32BE(0), 0x7FFFFFFF);
+
+      buffer = enc.encode(dpt,-2147483648);
+      assert.equal(buffer.readUInt32BE(0), 0x80000000);
+
+      buffer = enc.encode(dpt,-16);
+      assert.equal(buffer.readUInt32BE(0), 0xFFFFFFF0);
+    });
 
     it('should encode DPT14 values', function() {
-      var buffer = enc.encode(dpt14,40);
-      assert.equal(buf2str(buffer), '42200000');
+      buffer = enc.encode(dpt14,40);
+      assert.equal(buffer.readUInt32BE(0), 0x42200000);
 
       buffer = enc.encode(dpt14,35748613549);
-      assert.equal(buf2str(buffer), '51052C89');
+      assert.equal(buffer.readUInt32BE(0), 0x51052C89);
     });
 
     it('should encode DPT14 floating values', function() {
-      var buffer = enc.encode(dpt14,20.2);
-      assert.equal(buf2str(buffer), '41A1999A');
+      buffer = enc.encode(dpt14,20.2);
+      assert.equal(buffer.readUInt32BE(0), 0x41A1999A);
 
       buffer = enc.encode(dpt14,423.2584);
-      assert.equal(buf2str(buffer), '43D3A113');
+      assert.equal(buffer.readUInt32BE(0), 0x43D3A113);
     });
 
     it('should encode DPT14 negative values', function() {
-      var buffer = enc.encode(dpt14,-8787.84);
-      assert.equal(buf2str(buffer), 'C6094F5C');
+      buffer = enc.encode(dpt14,-8787.84);
+      assert.equal(buffer.readUInt32BE(0), 0xC6094F5C);
 
       buffer = enc.encode(dpt14,-14);
-      assert.equal(buf2str(buffer), 'C1600000');
+      assert.equal(buffer.readUInt32BE(0), 0xC1600000);
     });
 
-    it('should encode DPT15 values');
+    it('should encode DPT15 values', function() {
+      const dpt = tools.dptParse('DPT15');
+      buffer = enc.encode(dpt, {accessCode: 123456, readError: false, permission: true, encryption: false, index: 13});
+      assert.equal(buffer.readUInt32BE(0), 0b00010010001101000101011001001101);
+
+      buffer = enc.encode(dpt, {accessCode: 6789, readError: false, permission: false, readDirection: false, encryption: false, index: 14});
+      assert.equal(buffer.readUInt32BE(0), 0b00000000011001111000100100001110);
+    });
 
   });
 });
